08e261e7f844b50b53435aee253b304815782bee
yojina <yojinaaryal2064@gmail.com>
2025-11-17 21:07:27 +0545

    added user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const {Schema,model}=require("mongoose");
+
+const userSchema= new Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    password: {
+    type: String,
+    required: true,
+  
+    },
+    profileImageURL: {
+        type: String,
+        default: "/images/default.png",
+    },
+    role: {
+        type: String,
+        enum: ["USER","ADMIN"],
+        default: "USER",
+
+    }
+},
+{timestamps: true}
+);
+
+userSchema.pre("save",function(next){
+    const user = this;
+
+    if(!user.isModified("password")) return;
+})
+
+const User=model("user",userSchema);
+module.exports=User;
