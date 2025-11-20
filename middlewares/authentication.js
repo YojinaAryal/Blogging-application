3b65e1f613d05105b8755823487f7ab05932ee4e
yojina <yojinaaryal2064@gmail.com>
2025-11-20 21:38:16 +0545

    middleware added

---
+++ middlewares/authentication.js
@@ -0,0 +1,16 @@
+const { validateToken } = require("../service/authentication");
+
+function checkForAuthenticationCookie(cookieName){
+    return(req,res,next)=>{
+        const tokenCookieValue=req.cookie[cookieName]
+        if(!tokenCookieValue){
+            next();
+        }
+        try {
+             const userPayload=validateToken(tokenCookieValue);
+        } catch (error) {
+           
+        }
+         next();
+    }
+}
