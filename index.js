9596a5510d401fc04b9bd7a54cca9322f05a95ed
yojina <yojinaaryal2064@gmail.com>
2025-12-01 22:20:05 +0545

    image uploading

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const blogRoute= require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 const app=express();
 
+const Blog= require("./models/blog");
+
+
 
 const PORT = 8000;
 
@@ -20,11 +23,16 @@ app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")))
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{    
+    const allBlogs=await Blog.find({});
+    console.log(allBlogs);
     res.render("home",{
     user: req.user,
+    blogs: allBlogs,
     });
+
 });
 
 app.use("/user",userRoute)
