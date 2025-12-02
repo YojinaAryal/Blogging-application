8d29d02c09c133bdb65a7984462ab69e33128a66
yojina <yojinaaryal2064@gmail.com>
2025-12-02 21:29:40 +0545

    comment creation

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const multer=require("multer");
 const path = require("path");
 
 const Blog=require("../models/blog")
+const Comment=require("../models/comment")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,6 +21,23 @@ router.get("/add-new",(req,res)=>{
     return res.render("addBlog",{
         user: req.user,
     })
+})
+router.get("/:id",async(req,res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  console.log("blog",blog)
+  return res.render("blog", {
+    user: req.user,
+    blog,
+  });
+})
+
+router.post("/comment/:blogId",async(req,res )=>{
+  const comment= await Comment.create({
+    content: res.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 })
 router.post("/", upload.single("coverImage"), async (req,res)=>{
     const {title,body}=req.body
